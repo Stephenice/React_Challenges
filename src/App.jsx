3444a8782da541f694a3973e5a65e8eccfeee5ad
stephen ijeh <stephencode@hotmail.com>
2024-04-15 23:52:12 +0100

    Day 2 solution

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
-import Day_1 from "./day_1/Day_1"
+// import Day_1 from "./day_1/Day_1"
+import Day_2 from "./day_2/Day_2"
 
 
 export default function App() {
@@ -7,7 +8,7 @@ export default function App() {
 
     return (
         <div>       
-            <Day_1/>
+            <Day_2/>
         </div>
   )
 }
